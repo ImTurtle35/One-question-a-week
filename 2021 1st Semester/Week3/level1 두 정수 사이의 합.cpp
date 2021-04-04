7ff355d19ce4b4a776be7ec3428f1bbbf369910b
Kim Jeong Jin <52230120+ImTurtle35@users.noreply.github.com>
2021-04-04 16:20:38 +0900

    Create level1 두 정수 사이의 합.cpp

---
+++ 2021 1st Semester/Week3/level1 두 정수 사이의 합.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+long long solution(int a, int b)
+{
+    long long answer = 0;
+
+    if(a < b)
+    {
+        for (int i = a; i <= b; i++)
+        {
+            answer += i;
+        }
+    }
+    else if (b < a)
+    {
+        for (int i = b; i <= a; i++)
+        {
+            answer += i;
+        }
+    }
+    else if(a==b)
+        answer=a;
+    return answer;
+}
