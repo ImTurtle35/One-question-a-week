65adff307d3b4c4d9021546019ddfd752aaf121a
profrog <ache159@naver.com>
2021-05-23 17:13:53 +0900

    Update 삼각달팽이.cpp

--- Level2/2021-1/삼각달팽이.cpp
+++ Level2/2021-1/삼각달팽이.cpp
@@ -4,130 +4,105 @@
 
 using namespace std;
 
-class solute{
-
-	public:
-	
-	vector<vector<int>> answer0;
-	vector<int> answer00;
-	vector<int> answer;
-	
-	int data = 1;
-	int size;
-	int n;
-	int indexX = 0;
-	int indexY = 0;
+vector<vector<int>> answer0;
+vector<int> answer00;
+vector<int> answer;
+int size0 = 0;
+int indexX = 0;
+int indexY = 0;
+int data0 = 1;
 
 
-
-	void making_data()
+void print()
+{
+	for (int x = 0; x < answer0.size(); x++)
 	{
-		if(size > 0)
-			for (int x = 0 ; x < size ; x++)
-			{
-				answer0[indexX][indexY] = data;
-				data++;
-
-				indexX++;	
-			}
-
-		size--; //왼쪽
-
-		indexX--;
-		indexY++;
-		data--;
-
-		if (size > 0)
-			for (int x = 0; x < size; x++)
-			{
-				data++;
-
-				answer0[indexX][indexY] = data;
-				indexY++;
-			}
-
-		size--; //아래
-
-
-		indexX--;
-		indexY--;
-		indexY--;
-
-		if (size > 0)
-			for (int x = 0; x < size; x++)
-			{
-				data++;
-
-				answer0[indexX][indexY] = data;
-
-				//print();
-
-				indexX--;
-				indexY--;
-			}
-
-		//오른쪽
-		size--;
-		indexX++;
-		indexX++;
-		indexY++;
-		data++;
-	}
-
-
-	solute(int n0)
-	{
-		int x = 0;
-		size = n0;
-		int n = n0;
-
-		for (int x = 1; x <= n0; x++)
+		for (int y = 0; y < answer0[x].size(); y++)
 		{
-			vector<int> data;
-			data.assign(x, 0);
-
-			answer0.push_back(data);
+			//cout << answer0[x][y] << " ";
+			answer.push_back(answer0[x][y]);
 		}
-
-		int indexX = 0;
-		int indexY = 0;
-
-		while (size > 0) {
-			making_data();
-		}
-	
-		print();
+		//cout << endl;
 	}
+}
 
-	void print()
-	{
-		for (int x = 0; x < answer0.size(); x++)
+void making_data()
+{
+	if (size0 > 0)
+		for (int x = 0; x < size0; x++)
 		{
-			for (int y = 0; y < answer0[x].size(); y++)
-			{
-				//cout << answer0[x][y] << " ";
-				answer.push_back(answer0[x][y]);
-			}
-			//cout << endl;
-		}
-	}
+			answer0[indexX][indexY] = data0;
+			data0++;
 
-};
+			indexX++;
+		}
+
+	size0--; //왼쪽
+
+	indexX--;
+	indexY++;
+	data0--;
+
+	if (size0 > 0)
+		for (int x = 0; x < size0; x++)
+		{
+			data0++;
+
+			answer0[indexX][indexY] = data0;
+			indexY++;
+		}
+
+	size0--; //아래
+
+
+	indexX--;
+	indexY--;
+	indexY--;
+
+	if (size0 > 0)
+		for (int x = 0; x < size0; x++)
+		{
+			data0++;
+
+			answer0[indexX][indexY] = data0;
+
+			//print();
+
+			indexX--;
+			indexY--;
+		}
+
+	//오른쪽
+	size0--;
+	indexX++;
+	indexX++;
+	indexY++;
+	data0++;
+}
 
 
 vector<int> solution(int n) {
-	vector<int> answer;
-	solute *sol = new solute(n);
+	size0 = n;
 
-	return sol->answer;
+	for (int x = 1; x <= n; x++) //2array vector
+	{
+		vector<int> data;
+		data.assign(x, 0);
+		answer0.push_back(data);
+	}
+
+	while (size0 > 0) {
+		making_data();
+	}
+	
+	print();
+	return answer;
 }
 
 
 
 int main(void)
 {
-	solute *sol1 = new solute(5);
-	//sol1->print();
 	system("pause");
 	return 0;
 }
