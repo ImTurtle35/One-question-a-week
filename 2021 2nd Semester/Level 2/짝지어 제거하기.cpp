d8d3aec19f5d89155689989afba8ed5f956693ed
Shin Yu Mi <79843233+shinyumi43@users.noreply.github.com>
2021-09-05 18:16:26 +0900

    짝지어 제거하기

---
+++ 2021 2nd Semester/Level 2/짝지어 제거하기.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <stack>
+//using namespace std;
+//
+//int solution(string s) {
+//	stack<char> stk;
+//	for (int i = 0; i < s.length(); i++) {
+//		if (stk.empty() || s[i] != stk.top()) { stk.push(s[i]); }
+//		else if (s[i] == stk.top()) { stk.pop(); }
+//	}
+//
+//	if (stk.empty()) { return 1; }
+//	else { return 0; }
+//}
+//
+//int main() {
+//	string str;
+//	cin >> str;
+//	
+//	cout << solution(str) << endl;
+//}
