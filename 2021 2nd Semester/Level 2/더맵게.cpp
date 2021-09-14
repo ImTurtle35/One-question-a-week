7fa67f2626b070979a4dbacdfc7a99c9f67c8186
JJ.Sung <71392348+SKT-T1-BUSH@users.noreply.github.com>
2021-09-14 23:14:25 +0900

    Update 더맵게.cpp

--- 2021 2nd Semester/Level 2/더맵게.cpp
+++ 2021 2nd Semester/Level 2/더맵게.cpp
@@ -1,35 +1,35 @@
-//#include <iostream>
-//#include <vector>
-//#include <queue>
-//using namespace std;
-//
-//int solution(vector<int> scoville, int K) {
-//	int answer = 0;
-//	priority_queue<int, vector<int>, greater<int>> pq;
-//
-//	for (int i = 0; i < scoville.size(); i++) {
-//		pq.push(scoville[i]);
-//	}
-//
-//	while (pq.top() < K) {
-//		if (pq.size() == 1) { break;}
-//
-//		int min1 = pq.top(); 
-//		pq.pop();
-//		int min2 = pq.top() * 2;
-//		pq.pop();
-//
-//		pq.push(min1 + min2);
-//		answer++;
-//	}
-//
-//	if (pq.top() < K) { return -1; }
-//
-//	return answer;
-//}
-//
-//int main() {
-//	vector<int> sco = { 1,2,3,9,10,12 };
-//	int k = 7;
-//	cout << solution(sco, k) << endl;
-//}
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+	int answer = 0;
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	for (int i = 0; i < scoville.size(); i++) {
+		pq.push(scoville[i]);
+	}
+
+	while (pq.top() < K) {
+		if (pq.size() == 1) { break;}
+
+		int min1 = pq.top(); 
+		pq.pop();
+		int min2 = pq.top() * 2;
+		pq.pop();
+
+		pq.push(min1 + min2);
+		answer++;
+	}
+
+	if (pq.top() < K) { return -1; }
+
+	return answer;
+}
+
+int main() {
+	vector<int> sco = { 1,2,3,9,10,12 };
+	int k = 7;
+	cout << solution(sco, k) << endl;
+}
