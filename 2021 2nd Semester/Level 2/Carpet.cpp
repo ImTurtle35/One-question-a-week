b4bfc18aaf659122adb1fd3dad04c9e4be34975c
JJ.Sung <71392348+SKT-T1-BUSH@users.noreply.github.com>
2021-11-15 20:53:02 +0900

    Update Carpet.cpp

--- 2021 2nd Semester/Level 2/Carpet.cpp
+++ 2021 2nd Semester/Level 2/Carpet.cpp
@@ -1,19 +1,26 @@
-#include <vector>
- 
-using namespace std;
- 
-vector<int> solution(int brown, int red) {
-    vector<int> answer;
-    int sum = brown + red;
-    for (int height = 3; ; height++) {
-        if (!(sum % height)) {
-            int weight = sum / height;
-            if (((height - 2) * (weight - 2)) == red) {
-                answer.push_back(weight);
-                answer.push_back(height);
-                break;
-            }
-        }
-    }
-    return answer;
-}
+#include <string> 
+#include <vector> 
+#include <iostream> 
+using namespace std; 
+vector<int> 
+solution(int brown, int yellow) { 
+ vector<int> answer; 
+ for(int y = 1; y < 2500; y++) 
+   { for(int x = 1; x < 2500; x++) 
+     { if((brown - 4) / 2 == x + y && yellow == x * y) 
+       { if (x >= y) 
+         { answer.push_back(x + 2); 
+           answer.push_back(y + 2); 
+           return answer; 
+         } 
+         else 
+           { 
+             answer.push_back(y + 2);
+             answer.push_back(x + 2); 
+             return answer; 
+           }
+         } 
+       } 
+     }
+     return answer; 
+  }
